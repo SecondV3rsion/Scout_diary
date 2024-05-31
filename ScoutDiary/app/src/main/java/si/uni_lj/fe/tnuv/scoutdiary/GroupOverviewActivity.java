d40bea4f22fc3393bb9b48b0882b645097a81006
SecondV3rsion <Jan.kranjec12@gmail.com>
2024-05-31 13:59:51 +0200

    Now saving and loading flags

--- ScoutDiary/app/src/main/java/si/uni_lj/fe/tnuv/scoutdiary/GroupOverviewActivity.java
+++ ScoutDiary/app/src/main/java/si/uni_lj/fe/tnuv/scoutdiary/GroupOverviewActivity.java
@@ -1,11 +1,9 @@
 package si.uni_lj.fe.tnuv.scoutdiary;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -24,17 +22,15 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 public class GroupOverviewActivity extends AppCompatActivity {
 
     private ImageView groupImg;
     private TextView groupName;
-    private ImageButton btn_meetingImg;
-    private TextView DateView;
+    private ImageButton btnMeetingImg;
+    private TextView dateView;
     private ImageButton btnPreviousDay, btnNextDay;
     private EditText etMeetingName, etMeetingDescription;
     private RatingBar ratingBar;
@@ -43,8 +39,6 @@ public class GroupOverviewActivity extends AppCompatActivity {
     private ImageView flagRed, flagGreen, flagBlue, flagPurple, flagYellow;
     private Space flagsSpace;
 
-
-    // Class
     private PreferencesUtil preferencesUtil;
     private ImageSelector imageSelector;
 
@@ -59,9 +53,9 @@ public class GroupOverviewActivity extends AppCompatActivity {
         imageSelector = new ImageSelector(this, new ImageSelector.ImageSelectedCallback() {
             @Override
             public void onImageSelected(Uri uri) {
-                btn_meetingImg.setTag(uri.toString());
-                btn_meetingImg.setImageURI(uri);
-                saveDetailsForCurrentDate(); // Save the image URI when selected
+                btnMeetingImg.setTag(uri.toString());
+                btnMeetingImg.setImageURI(uri);
+                saveDetailsForCurrentDate();
             }
 
             @Override
@@ -70,7 +64,6 @@ public class GroupOverviewActivity extends AppCompatActivity {
             }
         });
 
-        // Initialize calendar and set the current date
         calendar = Calendar.getInstance();
         setCurrentDate();
         loadDetailsForCurrentDate();
@@ -79,15 +72,14 @@ public class GroupOverviewActivity extends AppCompatActivity {
     private void initViews() {
         groupImg = findViewById(R.id.slika_voda);
         groupName = findViewById(R.id.ime_voda);
-        btn_meetingImg = findViewById(R.id.btn_izberi_sliko_sestanka);
-        DateView = findViewById(R.id.current_date);
+        btnMeetingImg = findViewById(R.id.btn_izberi_sliko_sestanka);
+        dateView = findViewById(R.id.current_date);
         btnPreviousDay = findViewById(R.id.btn_previous_day);
         btnNextDay = findViewById(R.id.btn_next_day);
         etMeetingName = findViewById(R.id.et_meeting_name);
         etMeetingDescription = findViewById(R.id.et_meeting_description);
         ratingBar = findViewById(R.id.ocena_sestanka);
 
-        // Flags and space
         flagRed = findViewById(R.id.flag_red);
         flagGreen = findViewById(R.id.flag_green);
         flagBlue = findViewById(R.id.flag_blue);
@@ -96,7 +88,7 @@ public class GroupOverviewActivity extends AppCompatActivity {
     }
 
     private void setupListeners() {
-        btn_meetingImg.setOnClickListener(v -> imageSelector.openGallery());
+        btnMeetingImg.setOnClickListener(v -> imageSelector.openGallery());
         findViewById(R.id.btn_evidenca).setOnClickListener(v -> showAttendanceDialog());
         findViewById(R.id.btn_select_flags).setOnClickListener(v -> showFlagSelectionDialog());
 
@@ -122,7 +114,6 @@ public class GroupOverviewActivity extends AppCompatActivity {
         CheckBox checkboxYellow = dialog.findViewById(R.id.checkbox_yellow);
         Button btnSaveFlags = dialog.findViewById(R.id.btn_save_flags);
 
-        // Load current flag selections
         checkboxRed.setChecked(flagRed.getVisibility() == View.VISIBLE);
         checkboxGreen.setChecked(flagGreen.getVisibility() == View.VISIBLE);
         checkboxBlue.setChecked(flagBlue.getVisibility() == View.VISIBLE);
@@ -136,13 +127,12 @@ public class GroupOverviewActivity extends AppCompatActivity {
             flagPurple.setVisibility(checkboxPurple.isChecked() ? View.VISIBLE : View.GONE);
             flagYellow.setVisibility(checkboxYellow.isChecked() ? View.VISIBLE : View.GONE);
 
+            saveFlagsForCurrentDate();
             dialog.dismiss();
         });
 
         dialog.show();
     }
-
-
 
     private void showAttendanceDialog() {
         Dialog dialog = new Dialog(this);
@@ -154,7 +144,6 @@ public class GroupOverviewActivity extends AppCompatActivity {
         String dateKey = getDateKey();
         List<Boolean> attendance = preferencesUtil.loadAttendance(dateKey);
 
-        // Preveri, ali je seznam prisotnosti prazen in ga inicializiraj, če je potrebno
         if (attendance == null || attendance.isEmpty()) {
             attendance = new ArrayList<>(Collections.nCopies(members.size(), false));
         }
@@ -164,7 +153,7 @@ public class GroupOverviewActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         btnSave.setOnClickListener(v -> {
-            saveAttendance(adapter.getAttendanceList()); // Save attendance record
+            saveAttendance(adapter.getAttendanceList());
             Toast.makeText(this, R.string.prisotnost_shranjena, Toast.LENGTH_SHORT).show();
             dialog.dismiss();
         });
@@ -176,7 +165,6 @@ public class GroupOverviewActivity extends AppCompatActivity {
         String dateKey = getDateKey();
         preferencesUtil.saveAttendance(dateKey, attendance);
     }
-
 
     @Override
     protected void onResume() {
@@ -200,11 +188,11 @@ public class GroupOverviewActivity extends AppCompatActivity {
     private void setCurrentDate() {
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE, d. M. yy", Locale.getDefault());
         String currentDate = sdf.format(calendar.getTime());
-        DateView.setText(currentDate);
+        dateView.setText(currentDate);
     }
 
     private void changeDateBy(int days) {
-        saveDetailsForCurrentDate(); // Save details before moving to the next date
+        saveDetailsForCurrentDate();
         calendar.add(Calendar.DAY_OF_MONTH, days);
         setCurrentDate();
         loadDetailsForCurrentDate();
@@ -214,11 +202,11 @@ public class GroupOverviewActivity extends AppCompatActivity {
         String dateKey = getDateKey();
         String meetingName = etMeetingName.getText().toString();
         String meetingDescription = etMeetingDescription.getText().toString();
-        Uri meetingImageUri = btn_meetingImg.getTag() != null ? Uri.parse(btn_meetingImg.getTag().toString()) : null;
+        Uri meetingImageUri = btnMeetingImg.getTag() != null ? Uri.parse(btnMeetingImg.getTag().toString()) : null;
         float rating = ratingBar.getRating();
 
-        // Save meeting details including the image URI for the current date
-        preferencesUtil.saveMeetingDetails(dateKey, meetingName, meetingDescription, meetingImageUri, rating);
+        List<Boolean> flags = getFlags();
+        preferencesUtil.saveMeetingDetails(dateKey, meetingName, meetingDescription, meetingImageUri, rating, flags);
     }
 
     private void loadDetailsForCurrentDate() {
@@ -226,16 +214,43 @@ public class GroupOverviewActivity extends AppCompatActivity {
         etMeetingName.setText(preferencesUtil.loadMeetingName(dateKey));
         etMeetingDescription.setText(preferencesUtil.loadMeetingDescription(dateKey));
 
-        // Load the image URI for the current date directly from PreferencesUtil
         Uri meetingImageUri = preferencesUtil.loadMeetingImageUri(dateKey);
         if (meetingImageUri != null) {
-            btn_meetingImg.setTag(meetingImageUri.toString()); // Save the URI as a tag
-            btn_meetingImg.setImageURI(meetingImageUri);
+            btnMeetingImg.setTag(meetingImageUri.toString());
+            btnMeetingImg.setImageURI(meetingImageUri);
         } else {
-            btn_meetingImg.setTag(null); // Clear the tag if there's no image
-            btn_meetingImg.setImageResource(R.drawable.ic_launcher_foreground);
+            btnMeetingImg.setTag(null);
+            btnMeetingImg.setImageResource(R.drawable.ic_launcher_foreground);
         }
         ratingBar.setRating(preferencesUtil.loadMeetingRating(dateKey));
+        loadFlags(dateKey);
+    }
+
+    private void loadFlags(String dateKey) {
+        List<Boolean> flags = preferencesUtil.loadMeetingFlags(dateKey);
+        flagRed.setVisibility(flags.size() > 0 && flags.get(0) ? View.VISIBLE : View.GONE);
+        flagGreen.setVisibility(flags.size() > 1 && flags.get(1) ? View.VISIBLE : View.GONE);
+        flagBlue.setVisibility(flags.size() > 2 && flags.get(2) ? View.VISIBLE : View.GONE);
+        flagPurple.setVisibility(flags.size() > 3 && flags.get(3) ? View.VISIBLE : View.GONE);
+        flagYellow.setVisibility(flags.size() > 4 && flags.get(4) ? View.VISIBLE : View.GONE);
+    }
+
+    private void saveFlagsForCurrentDate() {
+        List<Boolean> flags = getFlags();
+        preferencesUtil.saveMeetingDetails(getDateKey(), etMeetingName.getText().toString(),
+                etMeetingDescription.getText().toString(),
+                btnMeetingImg.getTag() != null ? Uri.parse(btnMeetingImg.getTag().toString()) : null,
+                ratingBar.getRating(), flags);
+    }
+
+    private List<Boolean> getFlags() {
+        List<Boolean> flags = new ArrayList<>();
+        flags.add(flagRed.getVisibility() == View.VISIBLE);
+        flags.add(flagGreen.getVisibility() == View.VISIBLE);
+        flags.add(flagBlue.getVisibility() == View.VISIBLE);
+        flags.add(flagPurple.getVisibility() == View.VISIBLE);
+        flags.add(flagYellow.getVisibility() == View.VISIBLE);
+        return flags;
     }
 
     private String getDateKey() {
